cd36ae1e5b89c34698a807a4e378fa8f4c357255
Віталій Покрівчак <pokrivchakzvitalja@gmail.com>
2019-11-14 20:43:08 +0200

    add ChessServer

---
+++ Chess/IpClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Chess
+{
+    public class IpClient
+    {
+        static ManualResetEvent connectDone = new ManualResetEvent(false);
+        static ManualResetEvent sendDone = new ManualResetEvent(false);
+        public void SendMessage(string IP,int port,string msg)
+        {
+            var ip = new IPEndPoint(IPAddress.Parse(IP),port);
+
+                var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.BeginConnect(ip, new AsyncCallback(ConnectCallback), socket);
+                connectDone.WaitOne();
+                var byteData = Encoding.UTF8.GetBytes(msg);
+                Thread.Sleep(50);
+                socket.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallBack), socket);
+                sendDone.WaitOne();
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+
+        }
+        private static void SendCallBack(IAsyncResult ar)
+        {
+
+
+        Socket client = (Socket)ar.AsyncState;
+        int bytesSent = client.EndSend(ar);
+        sendDone.Set();
+
+        }
+
+        private static void ConnectCallback(IAsyncResult ar)
+        {
+        Socket client = (Socket)ar.AsyncState;
+        client.EndConnect(ar);
+        connectDone.Set();
+        }
+}
+}
---
+++ ChessServer/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ChessServer
+{
+    class Program
+    {
+        static Socket socketObj;
+        static string Recieve(Socket handler)
+        {
+            if (handler.Available != 0)
+            {
+                byte[] data = new byte[256];
+                int bytes = 0;
+                List<byte> Result = new List<byte>(256);
+                do
+                {
+                    bytes = handler.Receive(data, data.Length, SocketFlags.None);
+                    for (int i = 0; i < bytes; i++)
+                    {
+                        Result.Add(data[i]);
+                    }
+                } while (handler.Available > 0);
+
+                return Encoding.UTF8.GetString(Result.ToArray());
+            }
+            return "";
+        }
+        static void CallBack(Socket handler, string str)
+        {
+            handler.Send(Encoding.UTF8.GetBytes(str));
+        }
+
+        static void Main(string[] args)
+        {
+            socketObj =
+                new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint iPEnd = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5555);
+
+            socketObj.Bind(iPEnd);
+            socketObj.Listen(10);
+
+            bool stop = false;
+
+            while (!stop)
+            {
+                var handler = socketObj.Accept();
+
+                do
+                {
+                    Thread.Sleep(60);
+                } while (handler.Available == 0);
+
+                string msg = Recieve(handler);
+                Console.WriteLine(msg);
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+            socketObj.Shutdown(SocketShutdown.Both);
+            socketObj.Close();
+        }
+    }
+}
