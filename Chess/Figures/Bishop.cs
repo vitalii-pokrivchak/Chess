ea0607326890f6d83a9d25f63f369974f4787b21
Alexander Vasko <alexander.a.v.asko@gmail.com>
2019-11-18 19:52:09 +0200

    Added Bishop and Rook

--- Chess/Figures/Bishop.cs
+++ Chess/Figures/Bishop.cs
@@ -16,6 +16,47 @@ namespace Chess.Figures
             {
                 return MoveState.Cannot;
             }
+
+            Figure fig = deskGrid[newPos.X, newPos.Y];
+
+            int idx = 1;
+            int idy = 1;
+
+            if (newPos.X > Position.X && newPos.Y < Position.Y)
+            {
+                idy *= -1;
+            }
+
+            if (newPos.X < Position.X && newPos.Y > Position.Y)
+            {
+                idx *= -1;
+            }
+
+            if (newPos.X < Position.X && newPos.Y < Position.Y)
+            {
+                idx *= -1;
+                idy *= -1;
+            }
+
+            int x = Position.X + idx;
+            int y = Position.Y + idy;
+
+            for (; x < newPos.X && y < newPos.Y; x += idx, y += idy)
+            {
+                if (deskGrid[x, y] != null)
+                {
+                    return MoveState.Cannot;
+                }
+            }
+
+            if (x == newPos.X && y == newPos.Y)
+            {
+                if (deskGrid[x, y] != null)
+                {
+                    return MoveState.Fight;
+                }
+                return MoveState.Can;
+            }
             return MoveState.Cannot;
         }
     }
