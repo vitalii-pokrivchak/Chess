ec166da3cc63f47f421a61dc605c7c586f8db0b2
sabatex <serhiy.lakas@hotmail.com>
2020-06-24 21:37:01 +0300

    add chess js

--- WpfChess/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ WpfChess/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace WpfChess {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfChess;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfChess;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
